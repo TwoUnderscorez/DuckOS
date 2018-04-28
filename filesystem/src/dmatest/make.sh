aa06a3fff6c87a3f48c7d95a04fd2c433cc771de
TwoUnderscorez <ron.yutkin@gmail.com>
2018-04-28 14:21:52 +0300

    improve dmatest

--- filesystem/src/dmatest/make.sh
+++ filesystem/src/dmatest/make.sh
@@ -1,2 +1,2 @@
 gcc -c -Wall -Wno-discarded-qualifiers -Wno-unused-function -Wno-unknown-pragmas -fno-builtin -Wno-char-subscripts -fno-stack-protector -m32 program.c
-ld -T ../../lib/linker.ld -o ../../bin/dmatest -m elf_i386 ../../lib/libduck.o program.o
+ld -T ../../lib/linker.ld -o ../../bin/dmatest -m elf_i386 ../../lib/string.o ../../lib/libduck.o program.o
--- filesystem/src/dmatest/program.c
+++ filesystem/src/dmatest/program.c
@@ -1,12 +1,51 @@
 #include "../../lib/libduck.h"
+#include "../../lib/string.h"
+struct memory_block_header {
+    char used;
+    int length;
+    struct memory_block_header * next;
+} __attribute__((__packed__));
+typedef struct memory_block_header memory_block_header_t;
+
 void main(int argc, char ** argv);
+void disp_heap();
 
 void main(int argc, char ** argv) {
-    int * ptr = malloc(sizeof(int));
-    *ptr = 35;
-    puts("OK\n");
+    unsigned int * ptr = malloc(sizeof(int));
+    *ptr = 0xDDCCBBAA;
+    puts("Heap:\n");
     getc();
-    ptr = malloc(0x3000);
-    *ptr = 35;
-    puts("OK\n");
+    disp_heap();
+    ptr = malloc(0x1500);
+    *(unsigned int *)((unsigned int)ptr + 0x1450) = 0xDDCCBBAA;
+    puts("\nHeap:\n");
+    getc();
+    disp_heap();
+}
+
+void disp_heap() {
+    memory_block_header_t * heap_ptr = 0x700000;
+    unsigned int i;
+    unsigned char c;
+    if(!heap_ptr->used) return;
+    do {
+        set_screen_bgfg(0x70);
+        puts("Length: ");
+        screen_print_int(heap_ptr->length, 10);
+        puts("B; used: ");
+        if(heap_ptr->used) puts("yes");
+        else puts("no");
+        set_screen_bgfg(0x07);
+        for(i = 0; i < heap_ptr->length; i++){
+            set_screen_bgfg(0x07);
+            c = *((unsigned char *)((unsigned int)heap_ptr + sizeof(memory_block_header_t) + i));
+            screen_print_int(c, 16);
+            i++;
+            set_screen_bgfg(0x0F);
+            c = *((unsigned char *)((unsigned int)heap_ptr + sizeof(memory_block_header_t) + i));
+            screen_print_int(c, 16);
+        }
+        heap_ptr = heap_ptr->next;
+    } while(heap_ptr->length > 0);
+    set_screen_bgfg(0x07);
 }
