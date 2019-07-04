e6e9dd92d9ab327c275b8df6dc654f7f377c5c87
TwoUnderscorez <ron.yutkin@gmail.com>
2019-07-04 17:00:07 +0300

    Document bitmap

--- src/libs/bitmap.c
+++ src/libs/bitmap.c
@@ -1,50 +1,98 @@
 #include "bitmap.h"
 
-static int  get  (unsigned char,   unsigned char);
-static void set  (unsigned char *, unsigned char);
+static int get(unsigned char, unsigned char);
+static void set(unsigned char *, unsigned char);
 static void reset(unsigned char *, unsigned char);
 
-/* CAREFUL WITH pos AND BITMAP SIZE! */
+/*
+ copyright
+ https://codereview.stackexchange.com/questions/8213/bitmap-implementation
+ */
 
-int  bitmapGet(unsigned char *bitmap, int pos) {
-/* gets the value of the bit at pos */
-    return get(bitmap[pos/BIT], pos%BIT);
+/**
+ * @brief gets the value of the bit at pos 
+ * 
+ * @param bitmap
+ * @param pos 
+ * @return int 
+ */
+int bitmapGet(unsigned char *bitmap, int pos)
+{
+    return get(bitmap[pos / BIT], pos % BIT);
 }
 
-void bitmapSet(unsigned char *bitmap, int pos) {
-/* sets bit at pos to 1 */
-    set(&bitmap[pos/BIT], pos%BIT);
+/**
+ * @brief sets bit at pos to 1 
+ * 
+ * @param bitmap 
+ * @param pos 
+ */
+void bitmapSet(unsigned char *bitmap, int pos)
+{
+    set(&bitmap[pos / BIT], pos % BIT);
 }
 
-void bitmapReset(unsigned char *bitmap, int pos) {
-/* sets bit at pos to 0 */
-    reset(&bitmap[pos/BIT], pos%BIT);
+/**
+ * @brief sets bit at pos to 0
+ * 
+ * @param bitmap 
+ * @param pos 
+ */
+void bitmapReset(unsigned char *bitmap, int pos)
+{
+    reset(&bitmap[pos / BIT], pos % BIT);
 }
 
-int bitmapSearch(unsigned char *bitmap, int  n, int size, int start) {
-/* Finds the first n value in bitmap after start */
-/* size is the Bitmap size in unsigned chars */
+/**
+ * @brief 
+ * 
+ * @param bitmap Finds the first n value in bitmap after start
+ * size is the Bitmap size in unsigned chars
+ * @param n 
+ * @param size 
+ * @param start 
+ * @return int 
+ */
+int bitmapSearch(unsigned char *bitmap, int n, int size, int start)
+{
     int i;
     /* size is now the Bitmap size in bits */
-    for(i = start+1, size *= BIT; i < size; i++)
-        if(bitmapGet(bitmap,i) == n)
+    for (i = start + 1, size *= BIT; i < size; i++)
+        if (bitmapGet(bitmap, i) == n)
             return i;
     return BITMAP_NOTFOUND;
 }
 
-static int  get(unsigned char a, unsigned char pos) {
-/* pos is something from 0 to 7*/
+/**
+ * @brief get a bit from a char
+ * 
+ * @param a 
+ * @param pos 0<pos<7
+ * @return int 
+ */
+static int get(unsigned char a, unsigned char pos)
+{
     return (a >> pos) & 1;
 }
 
-static void set(unsigned char *a, unsigned char pos) {
-/* pos is something from 0 to 7*/
-/* sets bit to 1 */
+/**
+ * @brief set bit pos in a to 1
+ * 
+ * @param a 
+ * @param pos 0<pos<7
+ */
+static void set(unsigned char *a, unsigned char pos)
+{
     *a |= 1 << pos;
 }
 
-static void reset(unsigned char *a, unsigned char pos) {
-/* pos is something from 0 to 7*/
-/* sets bit to 0 */
+/**
+ * @brief set bit pos in a to 0
+ * 
+ * @param a 
+ * @param pos 0<pos<7
+ */
+static void reset(unsigned char *a, unsigned char pos)
+{
     *a &= ~(1 << pos);
 }
