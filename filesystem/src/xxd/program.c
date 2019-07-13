c5b4dd758872242747f51d03a914cb7536dbf2ab
TwoUnderscorez <ron.yutkin@gmail.com>
2019-07-13 15:44:15 +0300

    Fix xxd

--- filesystem/src/xxd/program.c
+++ filesystem/src/xxd/program.c
@@ -1,68 +1,77 @@
 #include "../../lib/libduck.h"
 #include "../../lib/string.h"
-void main(int argc, char ** argv);
-void phexDump (char *desc, void *addr, int len);
+void main(int argc, char **argv);
+void phexDump(char *desc, void *addr, int len);
 
-void main(int argc, char ** argv) {
-    if(argc > 1) {
+void main(int argc, char **argv)
+{
+    puts("hexdump\n");
+    if (argc > 1)
+    {
         int inode_num = path_to_inode(argv[1]);
-        EXT2_INODE_t * inode = malloc(sizeof(EXT2_INODE_t));
+        EXT2_INODE_t *inode = malloc(sizeof(EXT2_INODE_t));
         load_inode(inode_num, inode);
-        void * file_buff = malloc((inode->sector_usage+1)*512);
-        load_file(inode_num, 0, 0, file_buff);
+        void *file_buff = malloc(inode->size_low);
+        load_file(inode, 0, 0, file_buff);
         phexDump(argv[1], file_buff, inode->size_low);
     }
 }
 
-void phexDump (char *desc, void *addr, int len) {
+void phexDump(char *desc, void *addr, int len)
+{
     int i, j = 0;
     char buff[17], buff2[10];
-    unsigned char *pc = (unsigned char*)addr;
+    unsigned char *pc = (unsigned char *)addr;
 
     // Output description if given.
     if (desc)
         puts(desc);
-		puts(":");
+    puts(":");
 
-    if (!len) {
+    if (!len)
+    {
         puts("  ZERO LENGTH\n");
         return;
     }
-    if (len < 0) {
+    if (len < 0)
+    {
         puts("  NEGATIVE LENGTH\n");
         return;
     }
     // Process every byte in the data.
-    for (i = 0; i < len; i++) {
+    for (i = 0; i < len; i++)
+    {
         // Multiple of 16 means new line (with line offset).
 
-        if ((i % 16) == 0) {
+        if ((i % 16) == 0)
+        {
             // Just don't print ASCII for the zeroth line.
             if (i != 0)
-				puts (buff);
-				puts("\n");
-                j++;
-                if(j==24) {
-                    putc(' ');
-                    screen_set_bgfg(0x70);
-                    puts("--MORE--");
-                    screen_set_bgfg(0x07);
-                    getc();
-                    puts("\b\b\b\b\b\b\b\b\b\b");
-                    j=0;
-                }
+                puts(buff);
+            puts("\n");
+            j++;
+            if (j == 24)
+            {
+                putc(' ');
+                screen_set_bgfg(0x70);
+                puts("--MORE--");
+                screen_set_bgfg(0x07);
+                getc();
+                puts("\b\b\b\b\b\b\b\b\b\b");
+                j = 0;
+            }
 
             // Output the offset.
             // printf ("  %04x ", i);
             itoa(i, buff2, 16);
             puts(buff2);
-			puts(": ");
+            puts(": ");
         }
 
         // Now the hex code for the specific character.
         itoa(pc[i], buff2, 16);
         puts(buff2);
-		puts(" ");
+        puts(" ");
 
         // And store a printable ASCII character for later.
         if ((pc[i] < 0x20) || (pc[i] > 0x7e))
@@ -73,13 +82,14 @@ void phexDump (char *desc, void *addr, int len) {
     }
 
     // Pad out last line if not exactly 16 characters.
-    while ((i % 16) != 0) {
-        puts ("   ");
+    while ((i % 16) != 0)
+    {
+        puts("   ");
         i++;
     }
 
     // And print the final ASCII bit.
-    puts ("  ");
-	puts(buff);
-	puts("\n");
+    puts("  ");
+    puts(buff);
+    puts("\n");
 }
